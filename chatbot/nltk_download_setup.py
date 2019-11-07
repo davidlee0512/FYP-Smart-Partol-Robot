cb4141a3bcdb735f3bb5730450aba8551280000f
Lee Wai Leung <davidlee0512@yahoo.com.hk>
2019-11-08 00:15:43 +0800

    added a nltk download setup file

---
+++ chatbot/nltk_download_setup.py
@@ -0,0 +1,5 @@
+import nltk
+
+nltk.download('averaged_perceptron_tagger', download_dir="./nltk_data")
+nltk.download('maxent_ne_chunker', download_dir="./nltk_data")
+nltk.download('words', download_dir="./nltk_data")
