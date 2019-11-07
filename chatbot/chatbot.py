610a46fe3398812da46af4411831fb11a8bf460f
Lee Wai Leung <davidlee0512@yahoo.com.hk>
2019-11-08 00:03:27 +0800

    more testing on nltk

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,7 +1,12 @@
 import nltk
+from nltk.corpus import treebank
 
 #nltk.download('punkt')
-nltk.data.path += "\\nltk_data"
+nltk.data.path += ["./nltk_data"]
 
 output = nltk.word_tokenize(input("input a string >"))
 print(output)
+output2 = nltk.pos_tag(output)
+print(output2)
+output3 = nltk.chunk.ne_chunk(output2)
+print(output3)
