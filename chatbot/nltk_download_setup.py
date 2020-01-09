b70cb6e71c0020ad6a0ccd35c920132c47db3fc1
Feli <u3537471@connect.hku.hk>
2020-01-09 17:46:06 +0800

    update nltk setup

--- chatbot/nltk_download_setup.py
+++ chatbot/nltk_download_setup.py
@@ -1,5 +1,6 @@
 import nltk
 
+nltk.download('punkt', download_dir="./nltk_data")
 nltk.download('averaged_perceptron_tagger', download_dir="./nltk_data")
 nltk.download('maxent_ne_chunker', download_dir="./nltk_data")
 nltk.download('words', download_dir="./nltk_data")
