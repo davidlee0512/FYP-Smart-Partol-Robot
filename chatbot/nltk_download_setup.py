d6c99bc97ba9453a5345a6b64d4ce5c0ebe9d4c2
Lee Wai Leung <davidlee0512@yahoo.com.hk>
2020-01-12 08:33:04 +0800

    using word vector

--- chatbot/nltk_download_setup.py
+++ chatbot/nltk_download_setup.py
@@ -3,3 +3,4 @@ import nltk
 nltk.download('averaged_perceptron_tagger', download_dir="./nltk_data")
 nltk.download('maxent_ne_chunker', download_dir="./nltk_data")
 nltk.download('words', download_dir="./nltk_data")
+nltk.download('wordnet', download_dir="./nltk_data")
